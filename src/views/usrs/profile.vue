97d575495d5bbbd5a340de1e81efac18f306e1a3
Raúl Sánchez <raulszvz@users.noreply.github.com>
2019-04-06 13:08:25 -0600

    Version preliminar

--- src/views/usrs/profile.vue
+++ src/views/usrs/profile.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout text-xs-center align-start justify-center>
-    <v-card class="elevation-6">
+    <!--<v-card class="elevation-6">
       <v-card-text>
         <div class="ma-2">{{ usuario.name }}</div>
         <img class="ma-2 fotoPerfil" :src="require('@/assets/fotoUsuario.png')" alt="">
@@ -14,7 +14,60 @@
         <div class="ma-2">País: {{ usuario.country }}</div>
         <div class="ma-2">Telefono: {{ usuario.phone }}</div>
       </v-card-text>
-    </v-card>
+    </v-card>-->
+    <v-flex xs12 sm10 md6 lg8 xl4>
+    <template>
+      <v-expansion-panel>
+        <v-expansion-panel-content disabled>
+          <template v-slot:header>
+            <div>{{ usuario.name }}</div>
+          </template>
+          <v-card>
+            <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+        <v-expansion-panel-content>
+          <template v-slot:header>
+            <div>Institución</div>
+          </template>
+          <v-card>
+            <v-card-text><v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.institution }}</v-chip></v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+        <v-expansion-panel-content>
+          <template v-slot:header>
+            <div>Dirección</div>
+          </template>
+          <v-card>
+            <v-card-text>
+              <v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.street }} {{ usuario.number }}</v-chip>
+              <v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.colony }}</v-chip>
+              <v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.zipC }}</v-chip>
+              <v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.municipality }}</v-chip>
+              <v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.state }}</v-chip>
+              <v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.country }}</v-chip>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+        <v-expansion-panel-content>
+          <template v-slot:header>
+            <div>Contacto</div>
+          </template>
+          <v-card>
+            <v-card-text><v-chip color="orange" text-color="white"><v-avatar><v-icon>account_circle</v-icon></v-avatar>{{ usuario.phone }}</v-chip></v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+        <v-expansion-panel-content>
+          <template v-slot:header>
+            <div>Constancia</div>
+          </template>
+          <v-card>
+            <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </template>
+  </v-flex>
   </v-layout>
 </template>
 
--- src/views/usrs/usrhome.vue
+++ src/views/usrs/usrhome.vue
@@ -8,7 +8,7 @@
                     <v-toolbar-title>Resumenes</v-toolbar-title>
                     <v-divider class="mx-2" inset vertical></v-divider>
                     <v-spacer></v-spacer>
-                    <v-dialog v-model="dialog" max-width="500px">
+                    <v-dialog v-model="dialog" max-width="700px">
                         <template v-slot:activator="{ on }">
                             <v-btn color="primary" dark class="mb-2" v-on="on">Agregar</v-btn>
                         </template>
@@ -49,12 +49,28 @@
                                             <v-text-field label="Palabra clave" v-model="editedItem.keyword5"></v-text-field>
                                         </v-flex>
                                     </v-layout>
-                                    <v-layout row wrap>
+                                    <!--<v-layout row wrap>
                                         <v-flex xs12>
                                             <markdown-editor v-model="editedItem.content" ref="markdownEditor"></markdown-editor>
                                         </v-flex>   
+                                    </v-layout>-->
+                                    <v-layout row wrap>
+                                        <v-flex xs12>
+                                            <v-card>
+                                              <v-toolbar color="primary" dark card>
+                                                <v-toolbar-title>
+                                                  Cargar Archivo
+                                                </v-toolbar-title>        
+                                              </v-toolbar>
+                                              <v-card-text>  
+                                                <v-btn @click.native="selectFile" v-if="!uploadEnd && !uploading"> Subir archivo </v-btn>
+                                                {{ fileName }}
+                                                <input style="display: none" id="files" type="file" name="file" ref="uploadInput" accept=".pdf" :multiple="false" @change="detectFiles($event)" />
+                                                <!--<v-progress-circular v-if="uploading && !uploadEnd" :size="100" :width="15" :rotate="360" :value="progressUpload" color="primary"></v-progress-circular>-->
+                                              </v-card-text>
+                                            </v-card>
+                                        </v-flex>   
                                     </v-layout>
-
                                 </v-container>
                             </v-card-text>
                             <v-card-actions>
@@ -70,7 +86,7 @@
                     <v-data-table :headers="headers" :items="abstracts" hide-actions :pagination.sync="pagination" class="elevation-1">
                         <template v-slot:items="props">
                             <td class="text-xs-center">{{ props.item.title }}</td>
-                            <td class="text-xs-center">{{ props.item.date }}</td>
+                            <td class="text-xs-center">{{ props.item.date.seconds/1000 }}</td>
                             <td class="text-xs-center">{{ props.item.id }}</td>
                             <td class="justify-center">
                                 <!--<v-icon small @click="editItem(props.item)">visibility</v-icon>-->
@@ -94,14 +110,23 @@
 </template>
 
 <script>
+  import { auth ,db, storage } from '@/firebase'
   import { mapMutations } from 'vuex'
-  import { auth, db } from '@/firebase'
+  import uuidv4 from 'uuid/v4'
 
   export default {
     data: () => ({
       dialog: false,
       pagination: {},
       guardar: false,
+      progressUpload: 0,
+      filePdf: null,
+      fileName: '',
+      uploadTask: '',
+      uploading: false,
+      uploadEnd: false,
+      downloadURL: '',
+      user: '',
       headers: [
         { text: 'Titulo', align: 'center', sortable: true, value: 'title'},
         { text: 'Fecha', align: 'center', value: 'date', sortable: false },
@@ -112,6 +137,7 @@
       editedIndex: -1,
       editedItem: {
         id:'',
+        author:'',
         title:'',
         coauthor1:'',
         coauthor2:'',
@@ -120,10 +146,12 @@
         keyword3:'',
         keyword4:'',
         keyword5:'',
-        content:''
+        content:'',
+        file:''
       },
       defaultItem: {
         id:'',
+        author:'',
         title:'',
         coauthor1:'',
         coauthor2:'',
@@ -132,7 +160,8 @@
         keyword3:'',
         keyword4:'',
         keyword5:'',
-        content:''
+        content:'',
+        file:''
       }
     }),
 
@@ -152,7 +181,20 @@
     watch: {
       dialog (val) {
         val || this.close()
-      }
+      },
+      uploadTask: function () {
+      this.uploadTask.on('state_changed', sp => {
+        this.progressUpload = Math.floor(sp.bytesTransferred / sp.totalBytes * 100)
+      },
+      null,
+      () => {
+        this.uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+          this.uploadEnd = true
+          this.downloadURL = downloadURL
+          this.$emit('downloadURL', downloadURL)
+        })
+      })
+    }
     },
 
     created () {
@@ -163,6 +205,17 @@
       ...mapMutations(['mostrarOcupado', 'ocultarOcupado', 'mostrarExito', 'mostrarError','mostrarAdvertencia']),
       //...mapMutations('sesion', ['actualizarUsuario']),
 
+      selectFile () {
+        this.$refs.uploadInput.click()
+      },
+      detectFiles (e) {
+        this.filePdf = e.target.files || e.dataTransfer.files
+        this.fileName = this.filePdf[0].name
+        //this.mostrarExito(this.filePdf[0].name)
+        //Array.from(Array(this.filePdf.length).keys()).map(x => {
+          //this.upload(fileList[x])
+        //})
+      },  
       async consultarResumen(){
         let docs = await db.collection('abstract').get()
         docs.forEach(doc => {
@@ -178,18 +231,18 @@
         this.abstracts = []
       },
 
-      editItem (item) {
+      async editItem (item) {
         this.editedIndex = this.abstracts.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
       },
 
       deleteItem (item) {
-        //const index = this.abstracts.indexOf(item)
-        //confirm('¿Esta seguro de borrar esta entrada?') && this.abstracts.splice(index, 1)
+        let uid = auth.currentUser.uid
         this.editedIndex = this.abstracts.indexOf(item)
         this.editedItem = Object.assign({}, item)
         let ref = this.editedItem.id
+        storage.ref(`abstracts/${uid}/${ref}.pdf`).delete()
         db.collection("abstract").doc(ref).delete()
         this.mostrarExito('Entrada borrada')
         this.initialize()
@@ -204,10 +257,17 @@
       },
 
       async save () {
+        this.mostrarOcupado({ titulo: 'Guardando', mensaje: 'Espere mientras guardamos su entrada...' })
         let uid = auth.currentUser.uid 
-        let resumen = {
+        let archivoId = uuidv4()
+        try {
+          this.uploading = true
+          this.uploadTask = await storage.ref(`abstracts/${uid}/${archivoId}.pdf`).put(this.filePdf[0])
+          let url = await this.uploadTask.ref.getDownloadURL()
+          let resumen = {
             uid,  
-            id:'',
+            id: archivoId,
+            author: '',
             title: this.editedItem.title,
             coauthor1: this.editedItem.coauthor1,
             coauthor2: this.editedItem.coauthor2,
@@ -217,18 +277,28 @@
             keyword4: this.editedItem.keyword4,
             keyword5: this.editedItem.keyword5,
             content: this.editedItem.content,
+            file: url,
             date: new Date()
-             
         }
-        this.mostrarExito('Punto previo')
-        await db.collection('abstract').add(resumen)
-        .then(ref => {this.addID(ref.id)})
+        await db.collection('abstract').doc(archivoId).set(resumen)//.then(ref => {this.addID(ref.id)})
         this.mostrarExito('Entrada registrada')
         this.close()
         this.initialize()
+      }
+      catch (error) {
+        this.mostrarError(error)
+      }
+      finally {
+        this.ocultarOcupado()
+      }
       },
 
       async update () {
+        this.mostrarOcupado({ titulo: 'Actualizando', mensaje: 'Espere mientras actualizamos su entrada...' })
+        let ref = this.editedItem.id
+        let uid = auth.currentUser.uid
+        this.uploading = true
+        this.uploadTask = await storage.ref(`abstracts/${uid}/${ref}.pdf`).put(this.filePdf[0])
         let resumen = {
             title: this.editedItem.title,
             coauthor1: this.editedItem.coauthor1,
@@ -240,10 +310,9 @@
             keyword5: this.editedItem.keyword5,
             content: this.editedItem.content,
             date: new Date()
-             
         }
-        let ref = this.editedItem.id
         await db.collection('abstract').doc(ref).update(resumen)
+        this.ocultarOcupado()
         this.mostrarExito('Entrada actualizada')
         this.close()
         this.initialize()
